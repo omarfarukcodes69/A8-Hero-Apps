5aea364dd10b3ba285a69cf7417a4878fdb3355b
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-10-08 01:32:40 +0600

    navber features and design add

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { NavLink } from "react-router";
+import logo from "../../assets/logo.png"
 
 const Navbar = () => {
-    return (
-        <div>
-            this is Navber section
+  return (
+    <div className="navbar bg-base-100 shadow-sm  px-15">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <NavLink
+              to="/"
+              className={`hover:border-2 hover:border-[#5e24c3] hover:rounded-xl hover:px-3 hover:py-1 transform  ease-in-out`}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/apps"
+              className={`hover:border-2 hover:border-[#5e24c3] hover:rounded-xl hover:px-3 hover:py-1 transform  ease-in-out`}
+            >
+              Apps
+            </NavLink>
+            <NavLink
+              to="/installation"
+              className={`hover:border-2 hover:border-[#5e24c3] hover:rounded-xl hover:px-3 hover:py-1 transform  ease-in-out`}
+            >
+              Installation
+            </NavLink>
+          </ul>
         </div>
-    );
+        <div className="flex items-center">
+            <img className="max-w-10 max-h-10" src={logo} alt="Hero.oi logo" />
+            <a className="btn btn-ghost text-xl">Hero.OI</a>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 space-x-6 ">
+          <NavLink
+            to="/"
+            className={`hover:border-2 hover:border-[#5e24c3] hover:rounded-xl hover:px-3 hover:py-1 transform  ease-in-out`}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/apps"
+            className={`hover:border-2 hover:border-[#5e24c3] hover:rounded-xl hover:px-3 hover:py-1 transform  ease-in-out`}
+          >
+            Apps
+          </NavLink>
+          <NavLink
+            to="/installation"
+            className={`hover:border-2 hover:border-[#5e24c3] hover:rounded-xl hover:px-3 hover:py-1 transform  ease-in-out`}
+          >
+            Installation
+          </NavLink>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn bg-gradient-to-b from-[#723fe8] to-[#965AF0] text-white font-bold"><FaGithub className="text-xl" />Contribute</a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,16 +1,27 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../Pages/Home";
 import RootLayout from "../Layouts/RootLayout";
+import errorPage from "../Pages/errorPage";
+import AllApps from "../Pages/AllApps";
+import Installation from "../Pages/Installation";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
+    errorElement: errorPage,
     children: [
       {
-        path: "/",
+        index: true,
         Component: Home,
       },
+      {
+        path: "/apps",
+        Component: AllApps,
+      },{
+        path:'/installation',
+        Component:Installation,
+      }
     ],
   },
 ]);
