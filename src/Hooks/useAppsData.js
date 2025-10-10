551417542c7ffbd3924b4c5686c4538ca5634f9e
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-10-10 17:53:48 +0600

    public file call error hanle

--- src/Hooks/useAppsData.js
+++ src/Hooks/useAppsData.js
@@ -9,7 +9,7 @@ const useAppsData = () => {
 
     useEffect(() => {
         setLoading(true)
-        axios('../appsdata.json')
+        axios('/appsdata.json')
             .then(AppsData => setAppsData(AppsData.data))
             .catch(err => setError(err))
             .finally(() => setLoading(false))
